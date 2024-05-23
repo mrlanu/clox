35ecb3cb11a2dfa0b14919470a690fb8e6ed0b07
mrlanu <mrlanu@gmail.com>
2024-05-22 20:18:54 -0500

    feat: add chunk of instructions

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
 #include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
-int main(int argc, const char* argv[]) {
+int main(int argc, char const *argv[]) {
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_RETURN);
+  disassembleChunk(&chunk, "test_chunk");
+  freeChunk(&chunk);
   return 0;
 }
